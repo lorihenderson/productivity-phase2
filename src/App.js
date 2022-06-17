00b29c8d8e30ac4de5dbd4ea40f204d0631bd9af
James Kyan <james.kyan88@gmail.com>
2022-06-17 13:35:39 -0400

    dbsjon updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
 
+
+  useEffect(() => {
+    fetch("http://localhost:8000/posts/")
+      .then(response => response.json())
+      .then(data => console.log(data))
+  },[])
+  
+  return (
+    
+
+    
+    <div className="App">
     </div>
   );
 }
