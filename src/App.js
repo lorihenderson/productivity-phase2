b0d94a2414f24a14a60b72b33ac6b093db18f75b
James Kyan <james.kyan88@gmail.com>
2022-06-18 12:51:11 -0400

    new calendar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment'
 
 function App() {
 
-
+  const [dateState, setDateState] = useState(new Date())
+  const changeDate = (e) => {
+    setDateState(e)
+  }
+  
   useEffect(() => {
     fetch("http://localhost:8000/title")
-      .then(response => response.json())
-      .then(data => console.log("this is my", (data)))
+    .then(response => response.json())
+    .then(data => console.log("this is my", (data)))
   },[])
   
   return (
-    
-
-    
-    <div className="App">
-    </div>
+    <>
+      <Calendar 
+      value={dateState}
+      onChange={changeDate}
+      />
+    <p>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+    </>
   );
 }
 
